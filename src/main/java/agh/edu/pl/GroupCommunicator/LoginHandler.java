5f75f29b9539f541f5829e00b844156a89120a27
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-16 12:22:30 +0200

    login and registration handlers fixed

--- src/main/java/agh/edu/pl/GroupCommunicator/LoginHandler.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/LoginHandler.java
@@ -1,6 +1,7 @@
 package agh.edu.pl.GroupCommunicator;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -8,6 +9,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import tables.User;
+
 
 @WebServlet(name = "loginHandler", value = "/login-handler")
 public class LoginHandler extends HttpServlet{
@@ -18,11 +23,34 @@ public class LoginHandler extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String email = request.getParameter("email");
         if(email.isEmpty()){
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
-            requestDispatcher.include(request, response);
+            request.setAttribute("login_empty", true);
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
         }else{
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("mainpage.jsp");
-            requestDispatcher.forward(request, response);
+            Session session = Main.getSession();
+            User foundUser;
+            try {
+                Transaction tx = session.beginTransaction();
+                List<User> usersList = session.createQuery("from User as user where user.email=:userEmail", User.class)
+                        .setParameter("userEmail", email)
+                        .getResultList();
+                foundUser = usersList.isEmpty() ? null : usersList.get(0);
+                tx.commit();
+            } finally {
+                session.close();
+            }
+
+            if (foundUser == null ){
+                request.setAttribute("no_user", true);
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+            } else {
+                System.out.println(foundUser);
+                request.setAttribute("firstname", foundUser.getFirstname());
+                request.setAttribute("lastname", foundUser.getLastname());
+                request.setAttribute("address", foundUser.getAddress());
+                request.setAttribute("birthdate", foundUser.getBirthDate());
+                request.setAttribute("email", email);
+                request.getRequestDispatcher("/mainpage.jsp").forward(request, response);
+            }
         }
     }
 }
--- src/main/java/agh/edu/pl/GroupCommunicator/RegistrationHandler.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/RegistrationHandler.java
@@ -1,6 +1,5 @@
 package agh.edu.pl.GroupCommunicator;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 import tables.Address;
 import tables.User;
 import java.io.IOException;
@@ -20,17 +20,18 @@ public class RegistrationHandler extends HttpServlet{
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        Date birthDate = Date.valueOf(request.getParameter("birthdate"));
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         String email = request.getParameter("email");
         String street = request.getParameter("street");
         String city = request.getParameter("city");
         String zipCode = request.getParameter("zipcode");
-        if(firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || street.isEmpty() || city.isEmpty() || zipCode.isEmpty()){
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("register.jsp");
-            requestDispatcher.include(request, response);
-        }else{
+        if (firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || street.isEmpty()
+                || city.isEmpty() || zipCode.isEmpty() || request.getParameter("birthdate").isEmpty()){
+            request.setAttribute("empty_fields", true);
+            request.getRequestDispatcher("/register.jsp").forward(request, response);
+        } else {
+            Date birthDate = Date.valueOf(request.getParameter("birthdate"));
             Address address1 = new Address(street, city, zipCode);
             User user = new User(birthDate, firstname, lastname, email, address1);
             Session session = Main.getSession();
@@ -38,12 +39,15 @@ public class RegistrationHandler extends HttpServlet{
                 Transaction tx = session.beginTransaction();
                 session.save(user);
                 tx.commit();
+            } catch (ConstraintViolationException ex) {
+                request.setAttribute("constraint_exception", true);
+                request.getRequestDispatcher("/register.jsp").forward(request, response);
             } finally {
                 session.close();
             }
-
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
-            requestDispatcher.forward(request, response);
+            request.setAttribute("created_user", true);
+            request.setAttribute("user_email", email);
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
         }
     }
 }
