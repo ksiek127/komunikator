2c3268d5e4a1c315232fad4af823a90e0b4bb48f
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-19 10:39:53 +0200

    small refactoring, using User from Main

--- src/main/java/agh/edu/pl/GroupCommunicator/RemoveUserServlet.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/RemoveUserServlet.java
@@ -1,50 +1,33 @@
 package agh.edu.pl.GroupCommunicator;
 
-import agh.edu.pl.GroupCommunicator.tables.Mail;
 import agh.edu.pl.GroupCommunicator.tables.User;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.exception.ConstraintViolationException;
 
 import java.io.IOException;
-import java.util.List;
 
-@SuppressWarnings("unchecked")
-@WebServlet(name = "removeUser", value = "/removeUser")
-public class RemoveUser extends HttpServlet {
-
-    public RemoveUser() {
-        super();
-    }
+@WebServlet(name = "RemoveUserServlet", value = "/removeUser")
+public class RemoveUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String email = request.getParameter("email");
-
-        User user = null;
+        User user = Main.getUser();
 
         Transaction tx = null;
         Session session = Main.getSession();
         try {
             tx = session.beginTransaction();
-
-            Criteria criteria = session.createCriteria(User.class);
-            user = (User) criteria.add(Restrictions.eq("email", email)).uniqueResult();
-
             session.delete(user);
             tx.commit();
         } catch (Exception e) {
-            if(tx != null){
+            if (tx != null) {
                 tx.rollback();
             }
             e.printStackTrace();
@@ -52,7 +35,6 @@ public class RemoveUser extends HttpServlet {
             session.close();
         }
         request.setAttribute("removed_user", true);
-        request.setAttribute("user_email", email);
         request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
