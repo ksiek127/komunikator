344ccd32e294efc49c2ffaf728de4120d25dd764
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-19 10:36:30 +0200

    message and title string lengths fixed, added creation date column

--- src/main/java/agh/edu/pl/GroupCommunicator/tables/Mail.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/tables/Mail.java
@@ -1,16 +1,19 @@
 package agh.edu.pl.GroupCommunicator.tables;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 public class Mail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int mailID;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 1000)
     private String message;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 32)
     private String title;
+    @Column(nullable = false)
+    private Date created;
 
     public Mail() {
 
