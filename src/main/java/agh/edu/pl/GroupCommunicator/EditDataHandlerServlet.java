ab4d08475fe19428b0befb71bb490a389dd625c8
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-19 10:32:40 +0200

    refactoring, using User form Main, saving to database, catch fixed

--- src/main/java/agh/edu/pl/GroupCommunicator/EditDataHandlerServlet.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/EditDataHandlerServlet.java
@@ -1,43 +1,46 @@
 package agh.edu.pl.GroupCommunicator;
 
-import java.io.IOException;
-import java.sql.Date;
-
-
 import agh.edu.pl.GroupCommunicator.tables.Address;
 import agh.edu.pl.GroupCommunicator.tables.User;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.exception.ConstraintViolationException;
 
-@WebServlet(name = "editDataHandler", value = "/edit-data-handler")
-public class EditDataHandler extends HttpServlet{
-    public EditDataHandler() {
-        super();
-    }
+import javax.persistence.PersistenceException;
+import java.io.IOException;
+import java.sql.Date;
+
+@WebServlet(name = "EditDataHandlerServlet", value = "/edit-data-handler")
+public class EditDataHandlerServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         String email = request.getParameter("email");
         String street = request.getParameter("street");
         String city = request.getParameter("city");
         String zipCode = request.getParameter("zipcode");
+        String country = request.getParameter("country");
 
-        User user = null;
+        User user = Main.getUser();
 
+        if (user == null) {
+            request.setAttribute("email_first", true);
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        }
+
+        String oldZipCode = user.getAddress().getZipCode();
+        String oldEmail = user.getEmail();
 
         if (firstname.isEmpty() && lastname.isEmpty() && email.isEmpty() && street.isEmpty()
-                && city.isEmpty() && zipCode.isEmpty() && request.getParameter("birthdate").isEmpty()){
+                && city.isEmpty() && zipCode.isEmpty() && country.isEmpty() &&
+                request.getParameter("birthdate").isEmpty()) {
             request.setAttribute("empty_fields", true);
             request.getRequestDispatcher("/editdata.jsp").forward(request, response);
         } else {
@@ -45,9 +48,6 @@ public class EditDataHandler extends HttpServlet{
             Session session = Main.getSession();
             try {
                 Transaction tx = session.beginTransaction();
-
-                Criteria criteria = session.createCriteria(User.class);
-                user = (User) criteria.add(Restrictions.eq("email", request.getParameter("oldemail"))).uniqueResult();
 
                 if (!firstname.isEmpty()) {
                     user.setFirstname(firstname);
@@ -61,7 +61,7 @@ public class EditDataHandler extends HttpServlet{
                     user.setEmail(email);
                 }
 
-                if (!(street.isEmpty() && city.isEmpty() && zipCode.isEmpty())) {
+                if (!(street.isEmpty() && city.isEmpty() && zipCode.isEmpty() && country.isEmpty())) {
                     Address address = user.getAddress();
 
                     if (!street.isEmpty()) {
@@ -76,28 +76,31 @@ public class EditDataHandler extends HttpServlet{
                         address.setZipCode(zipCode);
                     }
 
+                    if (!country.isEmpty()) {
+                        address.setCountry(country);
+                    }
+
                     user.setAddress(address);
 
                 }
 
-                if (!request.getParameter("birthdate").isEmpty()){
+                if (!request.getParameter("birthdate").isEmpty()) {
                     Date birthDate = Date.valueOf(request.getParameter("birthdate"));
                     user.setBirthDate(birthDate);
                 }
 
+                session.update(user);
                 tx.commit();
-            } catch (ConstraintViolationException ex) {
+            } catch (PersistenceException ex) {
+                user.getAddress().setZipCode(oldZipCode);
+                user.setEmail(oldEmail);
                 request.setAttribute("constraint_exception", true);
                 request.getRequestDispatcher("/editdata.jsp").forward(request, response);
             } finally {
                 session.close();
             }
             request.setAttribute("edit_data_successful", true);
-            request.setAttribute("firstname", user.getFirstname());
-            request.setAttribute("lastname", user.getLastname());
-            request.setAttribute("address", user.getAddress());
-            request.setAttribute("birthdate", user.getBirthDate());
-            request.setAttribute("email", user.getEmail());
+            request.setAttribute("user", user);
             request.getRequestDispatcher("/mainpage.jsp").forward(request, response);
         }
     }
