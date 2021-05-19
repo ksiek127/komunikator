851243d0f5add10570576dec792caf9ea812f015
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-19 10:37:48 +0200

    using User from Main, added country field, small refactoring

--- src/main/java/agh/edu/pl/GroupCommunicator/RegistrationHandlerServlet.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/RegistrationHandlerServlet.java
@@ -1,5 +1,7 @@
 package agh.edu.pl.GroupCommunicator;
 
+import agh.edu.pl.GroupCommunicator.tables.Address;
+import agh.edu.pl.GroupCommunicator.tables.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,32 +10,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.exception.ConstraintViolationException;
-import agh.edu.pl.GroupCommunicator.tables.Address;
-import agh.edu.pl.GroupCommunicator.tables.User;
+
 import java.io.IOException;
 import java.sql.Date;
 
-@WebServlet(name = "registrationHandler", value = "/registration-handler")
-public class RegistrationHandler extends HttpServlet{
-    public RegistrationHandler() {
-        super();
-    }
+@WebServlet(name = "RegistrationHandlerServlet", value = "/registration-handler")
+public class RegistrationHandlerServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         String email = request.getParameter("email");
         String street = request.getParameter("street");
         String city = request.getParameter("city");
         String zipCode = request.getParameter("zipcode");
+        String country = request.getParameter("country");
+
         if (firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || street.isEmpty()
-                || city.isEmpty() || zipCode.isEmpty() || request.getParameter("birthdate").isEmpty()){
+                || city.isEmpty() || zipCode.isEmpty() || country.isEmpty() ||
+                request.getParameter("birthdate").isEmpty()) {
             request.setAttribute("empty_fields", true);
             request.getRequestDispatcher("/register.jsp").forward(request, response);
         } else {
             Date birthDate = Date.valueOf(request.getParameter("birthdate"));
-            Address address1 = new Address(street, city, zipCode);
+            Address address1 = new Address(street, city, zipCode, country);
             User user = new User(birthDate, firstname, lastname, email, address1);
             Session session = Main.getSession();
             try {
