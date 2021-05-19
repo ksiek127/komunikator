a56571724cd2331c294f40d57c1e2ab1f14fd913
Gabriel Cyganek <gabrielcyganek@gmail.com>
2021-05-19 10:35:48 +0200

    using User from Main, doGet added

--- src/main/java/agh/edu/pl/GroupCommunicator/LoginHandlerServlet.java
+++ src/main/java/agh/edu/pl/GroupCommunicator/LoginHandlerServlet.java
@@ -1,8 +1,6 @@
 package agh.edu.pl.GroupCommunicator;
 
-import java.io.IOException;
-import java.util.List;
-
+import agh.edu.pl.GroupCommunicator.tables.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,45 +8,57 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import agh.edu.pl.GroupCommunicator.tables.User;
+
+import java.io.IOException;
+import java.util.List;
 
 
-@WebServlet(name = "loginHandler", value = "/login-handler")
-public class LoginHandler extends HttpServlet{
-    public LoginHandler() {
-        super();
+@WebServlet(name = "LoginHandlerServlet", value = "/login-handler")
+public class LoginHandlerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User currentUser = Main.getUser();
+
+        if (currentUser == null) {
+            request.setAttribute("email_first", true);
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        } else {
+            request.setAttribute("user", currentUser);
+            request.getRequestDispatcher("/mainpage.jsp").forward(request, response);
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("email");
-        if(email.isEmpty()){
+        if (email.isEmpty()) {
             request.setAttribute("login_empty", true);
             request.getRequestDispatcher("/index.jsp").forward(request, response);
-        }else{
+        } else {
             Session session = Main.getSession();
             User foundUser;
+
             try {
                 Transaction tx = session.beginTransaction();
-                List<User> usersList = session.createQuery("from User as user where user.email=:userEmail", User.class)
+
+                List<User> usersList = session
+                        .createQuery("from User as user where user.email=:userEmail", User.class)
                         .setParameter("userEmail", email)
                         .getResultList();
                 foundUser = usersList.isEmpty() ? null : usersList.get(0);
+
                 tx.commit();
             } finally {
                 session.close();
             }
 
-            if (foundUser == null ){
+            if (foundUser == null) {
                 request.setAttribute("no_user", true);
                 request.getRequestDispatcher("/index.jsp").forward(request, response);
             } else {
-                System.out.println(foundUser);
-                request.setAttribute("firstname", foundUser.getFirstname());
-                request.setAttribute("lastname", foundUser.getLastname());
-                request.setAttribute("address", foundUser.getAddress());
-                request.setAttribute("birthdate", foundUser.getBirthDate());
-                request.setAttribute("email", email);
+                Main.setUser(foundUser);
+                request.setAttribute("user", foundUser);
                 request.getRequestDispatcher("/mainpage.jsp").forward(request, response);
             }
         }
